d9d9d43c5c8631a85d0a2429e5a24590b2a0e863
teasot <8462929+teasot@users.noreply.github.com>
2020-08-05 19:10:30 +1000

    Initial Commit
    Initial upload. Can rename pages programmatically, will add further functionality later

---
+++ FileHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PowerBIFileSDK
+{
+    internal static class FileHelper
+    {
+        internal static void PerformActionOnLayout(string FileLocation, Action<JObject> ActionToPerform)
+        {
+            using (FileStream ZipFileStream = new FileStream(FileLocation, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            using (ZipArchive Archive = new ZipArchive(ZipFileStream, ZipArchiveMode.Update))
+            {
+                JObject LayoutObject;
+                using (StreamReader Reader = new StreamReader(Archive.GetEntry(ArchiveFileLocations.Layout).Open(), System.Text.Encoding.Unicode))
+                using (JsonTextReader JsonReader = new JsonTextReader(new StringReader(Reader.ReadToEnd())))
+                {
+                    JsonReader.FloatParseHandling = FloatParseHandling.Decimal;
+
+                    LayoutObject = JObject.Load(JsonReader);
+
+                    ActionToPerform(LayoutObject);
+                }
+
+                Archive.GetEntry(ArchiveFileLocations.Layout).Delete();
+
+                System.Text.Encoding UnicodeWithoutBom = new System.Text.UnicodeEncoding(false, false);
+                byte[] LayoutByteArray = UnicodeWithoutBom.GetBytes(LayoutObject.ToString());
+
+                using (MemoryStream Stream = new MemoryStream(LayoutByteArray))
+                using (Stream Writer = Archive.CreateEntry(ArchiveFileLocations.Layout).Open())
+                    Stream.CopyTo(Writer);
+
+                Archive.GetEntry(ArchiveFileLocations.SecurityBindings)?.Delete();
+            }
+        }
+    }
+}
---
+++ PageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PowerBIFileSDK
+{
+    public static class PageHelper
+    {
+        public static void PerformActionOnEachPage(string FileLocation, Action<JObject> ActionToPerform) 
+        {
+            FileHelper.PerformActionOnLayout(
+                FileLocation, 
+                (JObject Layout) =>
+                {
+                    JArray PagesArray = (JArray)Layout.SelectToken(JsonPaths.Pages);
+                    foreach (JObject PageObject in PagesArray)
+                        ActionToPerform(PageObject);
+                }
+            );
+        }
+
+        public static void RenamePage(JObject PageObject, string PageName)
+        {
+            PageObject.SelectToken(JsonPaths.PageDisplayName).Replace(PageName);
+        }
+    }
+}
